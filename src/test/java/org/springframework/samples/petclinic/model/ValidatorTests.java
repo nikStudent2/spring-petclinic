47829a957a9f864a12a3474ad85c3df39f1485ab
nikStudent2 <nik.run@list.ru>
2018-11-11 17:33:14 +0300

    Update ValidatorTests.java

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -40,7 +40,7 @@ public class ValidatorTests {
         assertThat(constraintViolations.size()).isEqualTo(1);
         ConstraintViolation<Person> violation = constraintViolations.iterator().next();
         assertThat(violation.getPropertyPath().toString()).isEqualTo("firstName");
-        assertThat(violation.getMessage()).isEqualTo("1");
+        assertThat(violation.getMessage()).isEqualTo("must not be empty");
     }
 
 }
